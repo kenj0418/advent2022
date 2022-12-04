f9f1502fa18b778bedec057082cb53f8c2b796cd
Ken Johnson <kenj0418@gmail.com>
2022-12-04 10:20:54 -0800

    Day 4

--- day4.js
+++ day4.js
@@ -9,6 +9,14 @@ const isEitherContained = (ranges) => {
   return isContained(ranges[0], ranges[1]) || isContained(ranges[1], ranges[0]);
 }
 
+const isOverlapping = (ranges) => {
+  const r1 = ranges[0];
+  const r2 = ranges[1];
+
+  return (r1[0] <= r2[1] && r1[1] >= r2[0]) ||
+    (r2[0] <= r1[1] && r2[1] >= r1[0])
+}
+
 const run = () => {
   const allAssignments = readStringArrayFromFile("./input/day4.txt", "\n").map((line) => {
     return line.split(",").map((st) => {
@@ -19,8 +27,10 @@ const run = () => {
   });
 
   const contained = allAssignments.filter(isEitherContained);
+  const overlapping = allAssignments.filter(isOverlapping);
 
   console.log(`part 1: ${contained.length}`);
+  console.log(`part 2: ${overlapping.length}`);
 }
 
 module.exports = { run };
